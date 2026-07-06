698d919b6b753172988e0e8cdaa63dc46c33ce3b
jaspreet <jasspreet4640@gmail.com>
2026-07-06 00:59:13 -0500

    created vault file reading

--- SpaceExpedition/Program.cs
+++ SpaceExpedition/Program.cs
@@ -10,5 +10,20 @@ namespace SpaceExpedition
         {
             Console.WriteLine("WELCOME TO GALACTIC VAULT");
         }
+
+        static void LoadVault()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines("galactic_vault.txt");
+
+                Console.WriteLine("Vault file loaded.");
+                Console.WriteLine("Lines found: " + lines.Length);
+            }
+            catch
+            {
+                Console.WriteLine("Could not read galactic_vault.txt");
+            }
+        }
     }
 }
