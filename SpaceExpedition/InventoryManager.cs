95609e56d908734c6989447362cb2f59ab4ccb61
jaspreet <jasspreet4640@gmail.com>
2026-07-06 15:47:34 -0500

    Added LoadVault method

--- SpaceExpedition/InventoryManager.cs
+++ SpaceExpedition/InventoryManager.cs
@@ -7,5 +7,20 @@ namespace SpaceExpedition
     {
         Artifact[] artifacts = new Artifact[100];
         int count = 0;
+
+        public void LoadVault()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines("galactic_vault.txt");
+
+                Console.WriteLine("Vault file processing");
+                Console.WriteLine("Lines found: " + lines.Length);
+            }
+            catch
+            {
+                Console.WriteLine("File not found");
+            }
+        }
     }
 }
